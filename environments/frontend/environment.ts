a49ca8f548073b2702d392522154db5ecd37cf50
Gui Seek <guiseek@gmail.com>
2019-11-20 23:51:55 -0300

    remove firebase config

--- environments/frontend/environment.ts
+++ environments/frontend/environment.ts
@@ -2,18 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+require('dotenv').config();
+
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3333',
-  firebase: {
-    apiKey: "AIzaSyDmtOHbQSOecFCI_-quY5mInR9B0DXbzdk",
-    authDomain: "gui-seek.firebaseapp.com",
-    databaseURL: "https://gui-seek.firebaseio.com",
-    projectId: "gui-seek",
-    storageBucket: "gui-seek.appspot.com",
-    messagingSenderId: "911805937758",
-    appId: "1:911805937758:web:a1e922ded425c628"
-  },
+  firebase: process.env.FIREBASE
 };
 
 /*
